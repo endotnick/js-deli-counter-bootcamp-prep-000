f5d16b4ad49dcb99caa9f9e803038513a38a8456
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-10-12 23:11:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const katzDeliLine = [];
 
 const takeANumber = (line, name) => {
-    return line.push(name); 
+    line.push(name);
+    return line;
 };
 
 const nowServing = (line) => {
-  return line.unshift();  
+  return line.unshift();
 };
 
 const currentLine = (line) => {
   return katzDeliLine.toString();
 };
-
-
